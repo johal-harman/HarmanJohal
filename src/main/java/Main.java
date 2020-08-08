ad99273b78628e5e5e159b60aea591833977ee36
khokher-poonam <khokherpoonam13@gmail.com>
2020-08-08 15:27:03 -0400

    August Changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,21 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.testng.reporters.jq.BasePanel.C;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello harman");
-        System.out.println("hello vivek");
-        System.out.println("hello poonam");
+WebDriver driver;
+
+    @Test
+
+    public void setUp()
+
+    {
+
+        System.setProperty("WebDriver.ChromeDriver","C:\\Automation\\chromedriver.exe");
+
+driver=new ChromeDriver();
+        driver.get("https://www.google.com/");
     }
 }
